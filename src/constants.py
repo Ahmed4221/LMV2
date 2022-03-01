5eb7a7f84ca0bf0ae422eb1ed13239ce7da7590d
MIK <moh.ibrahimkhan.mike@gmail.com>
2022-03-01 14:42:25 +0500

    Add processed images and json

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+import os
+ALLOWED_EXTENSIONS = ['pdf']
+
+if not os.path.exists('data/'):
+    os.mkdir('data/')
+RAW_DATA_PATH = 'data/raw_data'
+if not os.path.exists(RAW_DATA_PATH):
+    os.mkdir(RAW_DATA_PATH)
+PROCESSED_DATA_PATH = 'data/processed_data'
+if not os.path.exists(PROCESSED_DATA_PATH):
+    os.mkdir(PROCESSED_DATA_PATH)
+PROCESSED_DATA_JSON_PATH = 'data/processed_json'
+if not os.path.exists(PROCESSED_DATA_JSON_PATH):
+    os.mkdir(PROCESSED_DATA_JSON_PATH)
+DRAW = False
